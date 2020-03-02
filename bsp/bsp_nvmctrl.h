cc6c3e10ec7ae7d5c77a36535b10cb12e52f4cf1
wallarug <wallarug@coliemore.com.au>
2020-03-02 19:23:31 +1100

    bsp_nvmctrl (fix): eeprom_read() set with equiv registers for SAMD51

--- bsp/bsp_nvmctrl.h
+++ bsp/bsp_nvmctrl.h
@@ -59,15 +59,12 @@ static void eeprom_read(uint8_t addr, uint8_t *buf, uint8_t size)
 
 #ifndef SAMD51
 	while (!NVMCTRL->INTFLAG.bit.READY);
-
-	/* Clear flags */
-	NVMCTRL->STATUS.reg = NVMCTRL_STATUS_MASK;
 #else
-	while (!NVMCTRL->INTFLAG.bit.READY);
+	while (!NVMCTRL->STATUS.bit.READY);
+#endif
 
 	/* Clear flags */
 	NVMCTRL->STATUS.reg = NVMCTRL_STATUS_MASK;
-#endif
 
 	/* Check whether byte address is word-aligned*/
 	if (src_addr % 2) {
