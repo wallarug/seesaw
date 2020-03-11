a74c627d3fec1aa1cb86b2c9d628480bc8871126
wallarug <wallarug@coliemore.com.au>
2020-03-11 21:52:35 +1100

    bsp_adc.h: adc_trigger().

--- bsp/bsp_adc.h
+++ bsp/bsp_adc.h
@@ -9,6 +9,8 @@ void adc_set_freerunning(bool mode);
 #ifndef SAMD51
 inline void syncADC() { while (ADC->STATUS.bit.SYNCBUSY == 1); }
 inline void adc_trigger(){ syncADC(); ADC->SWTRIG.bit.START = 1;}
+#else
+inline void adc_trigger(){ while( ADC0->SYNCBUSY.reg & ADC_SYNCBUSY_ENABLE ); ADC0->SWTRIG.bit.START = 1;}
 #endif
 
 void adc_set_inputscan(uint8_t channels);
